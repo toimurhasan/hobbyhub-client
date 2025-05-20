1158ffed071444caa1835000c23228e268b93717
toimurhasan <2imur.hasan@gmail.com>
2025-05-20 16:21:37 +0600

    Login page added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,8 +2,22 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="h-screen flex flex-col justify-center">
+      <div className="card bg-base-100 mx-auto w-full max-w-sm shrink-0 shadow-2xl">
+        <div className="card-body">
+          <h1 className="text-2xl text-center font-semibold">Login</h1>
+          <fieldset className="fieldset">
+            <label className="label">Email</label>
+            <input type="email" className="input w-full" placeholder="Email" />
+            <label className="label">Password</label>
+            <input type="password" className="input w-full" placeholder="Password" />
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+            <button className="btn btn-neutral mt-4">Login</button>
+          </fieldset>
+        </div>
+      </div>
     </div>
   );
 };
