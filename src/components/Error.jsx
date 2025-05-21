85b08e7b02214c0614cec10b4814ea9935fef109
toimurhasan <2imur.hasan@gmail.com>
2025-05-21 14:20:54 +0600

    error page customization

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Error = () => {
   return (
-    <div>
+    <div className="h-screen flex flex-col justify-center items-center text-gray-600 italic">
       <h1>404 - Page Not Found</h1>
       <p>The page you're looking for doesn't exist.</p>
     </div>
