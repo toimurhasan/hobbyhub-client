75ea70a8949dc4d2581ddff086f389a92a3bfb52
toimurhasan <2imur.hasan@gmail.com>
2025-05-21 16:20:41 +0600

    added features and testimonial components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Banner from "./Banner";
+import Testimonial from "./Testimonial";
+import Features from "./Features";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
       <Banner />
+      <Features />
+      <Testimonial />
     </div>
   );
 };
