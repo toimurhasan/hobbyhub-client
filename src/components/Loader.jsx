d33776d81971787c544380a597a2da6dc887ced2
toimurhasan <2imur.hasan@gmail.com>
2025-05-21 14:14:29 +0600

    added loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="flex justify-center">
+      <span className="loading loading-spinner loading-xl"></span>
+    </div>
+  );
+};
+
+export default Loader;
