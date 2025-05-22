788e87a75bfa6da969aa419922724067825604e0
toimurhasan <2imur.hasan@gmail.com>
2025-05-22 11:38:17 +0600

    added onAuthStateChanged function

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { AuthContext } from "./AuthContext";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase/firebase.config";
 
 const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setCurrentUser(user);
@@ -10,8 +15,12 @@ const AuthProvider = ({ children }) => {
 
     return unsubscribe; // cleanup
   }, []);
-  const userInfo = {};
-  return <AuthContext value={userInfo}>{children}</AuthContext>;
+
+  const userInfo = {
+    currentUser,
+  };
+
+  return <AuthContext value={userInfo}>{!loading && children}</AuthContext>;
 };
 
 export default AuthProvider;
