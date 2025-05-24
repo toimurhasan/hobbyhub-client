f57cc3b355ea54b62ab379d027de13b66fd41cdd
toimurhasan <2imur.hasan@gmail.com>
2025-05-24 07:02:50 +0600

    images updated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,16 +5,16 @@ const Banner = () => {
   return (
     <Swiper spaceBetween={50} slidesPerView={1}>
       <SwiperSlide>
-        <img src="/one.jpg" />
+        <img src="https://i.ibb.co/N2972b87/debby-hudson-Gk-Zoz3g-VG8c-unsplash-min.jpg" />
       </SwiperSlide>
       <SwiperSlide>
-        <img src="/two.jpg" />
+        <img src="https://i.ibb.co/7N0Trxy6/priscilla-du-preez-Pu-Zg-Wp-a0-Cs-unsplash-min.jpg" />
       </SwiperSlide>
       <SwiperSlide>
-        <img src="/three.jpg" />
+        <img src="https://i.ibb.co/dJbFdjbh/engin-akyurt-ZA31-Flg81-RE-unsplash-min.jpg" />
       </SwiperSlide>
       <SwiperSlide>
-        <img src="/four.jpg" />
+        <img src="https://i.ibb.co/Kc8STxkt/impressive-inspirations-6-IMOwz-Pds-Kg-unsplash-min.jpg" />
       </SwiperSlide>
     </Swiper>
   );
